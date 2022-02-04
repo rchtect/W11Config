0bcf40b9e4d084bd7a9113f360d0fad219178872
rchtect <90619276+rchtect@users.noreply.github.com>
2022-02-04 15:53:08 +0000

    Removed RoundedTB, updated user-profile.ps1

--- .config/powershell/user-profile.ps1
+++ .config/powershell/user-profile.ps1
@@ -1,11 +1,13 @@
 #Init
-Clear
 Function reboot {shutdown /r /t 0}
 Function poweroff {shutdown /s /t 0}
 Function sleepnow {rundll32.exe powrprof.dll, SetSuspendState Sleep}
 Import-Module posh-git
 Import-Module oh-my-posh
+Import-Module Terminal-Icons
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PoshPrompt -Theme "AtomicBit(modded)"
 Set-Alias -name neofetch -value winfetch
 Set-Alias -name restart -value $PROFILE
-Set-Alias -name quit -value exit
