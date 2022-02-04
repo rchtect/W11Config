05cd9d4b62ac7c60787ed669040eb76beae1645e
rchtect <90619276+rchtect@users.noreply.github.com>
2022-02-04 14:23:56 +0000

    Update user-profile.ps1

--- .config/powershell/user-profile.ps1
+++ .config/powershell/user-profile.ps1
@@ -1,5 +1,11 @@
 #Init
 Clear
+Function reboot {shutdown /r /t 0}
+Function poweroff {shutdown /s /t 0}
+Function sleepnow {rundll32.exe powrprof.dll, SetSuspendState Sleep}
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-PoshPrompt -Theme "AtomicBit(modded)"
+Set-Alias -name neofetch -value winfetch
+Set-Alias -name restart -value $PROFILE
+Set-Alias -name quit -value exit
